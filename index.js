f4fd4b62670a19bcec7720fcbd1ec047fafa3ef0
fernandotonon <tonon.fernando@hotmail.com>
2023-07-22 15:38:32 -0400

    fix test error

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ module.exports = {
     rules: {
       'no-commente-code': require('./lib/rules/no-commented-code'),
     },
-    test: {
-        'no-commented-code': require('./lib/tests/no-commented-code.test'),
-    }
   };
   
